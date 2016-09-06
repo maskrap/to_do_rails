2e958694aa1d5a03a8a6142fb2e944d40a18f157
Sam Park <sampark18@gmail.com>
2016-09-05 21:54:01 -0700

    I has no idea how to add remaining CRUD functionality at this point

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,6 +14,30 @@ class TasksController < ApplicationController
     end
   end
 
+
+# NO IDEAA WHAT I'M DOING BELOW THIS POINT
+
+  # def edit
+  #   @list = List.find(params[:id])
+  #   render :edit
+  # end
+  # 
+  # def update
+  #   @list = List.find(params[:id])
+  #   if @task.update(task_params)
+  #     redirect_to lists_path(@task.list)
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @list = List.find(params[:id])
+  #   @list.destroy
+  #   redirect_to lists_path
+  # end
+
+
 private
   def task_params
     params_require(:task).permit(:description)
