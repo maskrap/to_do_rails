4a5c2961824248e44d5f510661d1bf4591580ace
Cory Olson and Sam Park <cory+sam@>
2016-09-06 16:17:50 -0700

    Refactoring and adding capybara

---
+++ spec/features/add_list_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe "the add a list process" do
+  it "adds a new list" do
+    visit lists_path
+    click_link 'New List'
+    fill_in 'Name', :with => 'Things to do tonight'
+    click_on 'Create List'
+    expect(page).to have_content 'Your mother'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_list_path
+    click_on 'Create List'
+    expect(page).to have_content 'errors'
+  end
+end
---
+++ spec/features/edit_task_pages_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "the edit a task process" do
+  it "edits a task" do
+    list = List.create(:name => 'Things to do tonight')
+    task = Task.create(:description => 'Your mother', :list_id => list_id)
+    visit list_path(list)
+    click_on 'Edit'
+    fill_in 'Description', :with => 'Wash the dog'
+    click_on 'Update Task'
+    expect(page).to have_content 'Wash the dog'
+  end
+  it "gives error when no description is entered" do
+      list = List.create(:name => 'Home stuff')
+      task = Task.create(:description => 'Wash the dishes', :list_id => list.id)
+      visit list_path(list)
+      click_on 'Edit'
+      fill_in 'Description', :with => ''
+      click_button 'Update Task'
+      expect(page).to have_content 'errors'
+    end
+end
